a6a246d6595f4ed2236c41db5e7dffd08d192cb3
Marcos <marcosvto1@gmail.com>
2021-02-15 21:00:48 -0300

    Interagindo com listas

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Balta.ContentContext;
 
 namespace Balta
@@ -15,11 +16,39 @@ namespace Balta
       articles.Add(new Article("Artigo sobre Angular", "angular"));
       articles.Add(new Article("Artigos sobre c#", "csharp"));
 
-      foreach (var article in articles)
-      {
-        Console.WriteLine(article.Id);
-        Console.WriteLine(article.Title);
-        Console.WriteLine(article.Url);
+      // foreach (var article in articles)
+      // {
+      //   Console.WriteLine(article.Id);
+      //   Console.WriteLine(article.Title);
+      //   Console.WriteLine(article.Url);
+      // }
+
+      var courses = new List<Course>();
+      var courseOOP = new Course("Fundamentos OOP", "fundamentos-oop");
+      var courseAspNet = new Course("Fundamentos ASP.NET", "fundamento ASP.NET");
+      var courseCSharp = new Course("Fundamentos CSharp", "fundamento Csharp");
+
+      courses.Add(courseOOP);
+      courses.Add(courseAspNet);
+      courses.Add(courseCSharp);
+
+      var careers = new List<Career>();
+      var careerDotNet = new Career("Especialista .NET", "especialista-dotnet");
+      var careerItem3 = new CareerItem(3, "Fudanmentos APS.NET", "", courseAspNet);
+      var careerItem = new CareerItem(1, "Começe por aqui", "", null);
+      var careerItem2 = new CareerItem(2, "Fudanmentos C#", "", courseCSharp);
+
+      careerDotNet.Items.Add(careerItem3);
+      careerDotNet.Items.Add(careerItem);
+      careerDotNet.Items.Add(careerItem2);
+
+      careers.Add(careerDotNet);
+
+      foreach (var carreer in careers) {
+        System.Console.WriteLine($">>{carreer.Title}");
+        foreach (var item in carreer.Items.OrderBy(x => x.Order)) {
+          System.Console.WriteLine($"{item.Order}-{item.Title}");
+        }
       }
 
     }
